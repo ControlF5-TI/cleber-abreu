67079c95508b4136f402715efb4acde1e54f890e
Cleber Freitas de Abreu da Silva <clfsilva@banestes.com.br>
2024-10-03 17:23:59 -0300

    Initial version

---
+++ App/Models/Product.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ProductApp.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+
+        [Required()]
+        [StringLength(20)]
+        public required string Name { get; set; }
+
+        [Required()]
+        [StringLength(100)]
+        public required string Description { get; set; }
+
+        [Required()]
+        [Column(TypeName = "decimal(18,2)")]
+        public decimal Price { get; set; }
+
+        [Required()]
+        public int Total { get; set; }
+    }
+}
